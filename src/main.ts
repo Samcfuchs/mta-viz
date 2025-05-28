f6898544aeb2762e1233d6aebe38eb09e2c0193b
Sam Fuchs <samcfuchs@gmail.com>
2025-05-28 14:21:49 -0400

    Add new realtime data server

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,24 @@ initScene();
 //await initData()
 await initDataTracks();
 
+const REALTIME_URL = 'http://localhost:3000/realtime'
+
 
 init().then(() => {
     (async function fetchData() {
         try {
-            const d = await pull();
-            console.info('rtd loaded:', d)
-            if (d) setData(d);
+            //const d = await fetch(DATA_SERVER + '/realtime')
+            const d = pull();
+            
+            const data = await fetch(REALTIME_URL).then(response => {
+                if (!response.ok) throw new Error("Fetching realtime data failed");
+                return response.json();
+            })
+
+            console.debug("data loaded:", data);
+
+            console.debug('rtd loaded:', d);
+            if (data) setData(data);
         } catch (error) {
             console.error("Error fetching data:", error)
         } finally {
